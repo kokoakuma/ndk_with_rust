5ea847b72cf527240ebe142b3e0876b9a792a04a
kokoakuma <aikw9101@gmail.com>
2023-03-16 23:51:00 +0900

    add int array fun

--- app/src/main/java/com/kokoadev/ndkwithrust/MainActivity.kt
+++ app/src/main/java/com/kokoadev/ndkwithrust/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.kokoadev.ndkwithrust
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.kokoadev.ndkwithrust.databinding.ActivityMainBinding
 
@@ -18,6 +19,9 @@ class MainActivity : AppCompatActivity() {
 //        binding.sampleText.text = stringFromJNI()
 //        binding.sampleText.text = helloNameFromJNI("kokoa")
         binding.sampleText.text = addNumberFromJNI(1,2).toString()
+
+        val result = sortIntArrayFromJNI(listOf(5,4,3,2,1).toIntArray())
+        binding.sampleText.text = result.toList().joinToString()
     }
 
     /**
@@ -25,8 +29,10 @@ class MainActivity : AppCompatActivity() {
      * which is packaged with this application.
      */
     external fun stringFromJNI(): String
+
     external fun helloNameFromJNI(input: String): String
     external fun addNumberFromJNI(input1: Int, input2: Int): Int
+    external fun sortIntArrayFromJNI(input: IntArray): IntArray
 
     companion object {
         // Used to load the 'ndkwithrust' library on application startup.
--- app/src/main/rust/hellorust/src/lib.rs
+++ app/src/main/rust/hellorust/src/lib.rs
@@ -1,6 +1,6 @@
 use jni::JNIEnv;
-use jni::objects::{JObject, JString};
-use jni::sys::{jint, jstring};
+use jni::objects::{JObject, JString, ReleaseMode};
+use jni::sys::{jint, jintArray, jstring};
 
 #[no_mangle]
 pub unsafe extern "C" fn Java_com_kokoadev_ndkwithrust_MainActivity_stringFromJNI(
@@ -19,9 +19,9 @@ pub unsafe extern "C" fn Java_com_kokoadev_ndkwithrust_MainActivity_stringFromJN
 
 #[no_mangle]
 pub unsafe extern "C" fn Java_com_kokoadev_ndkwithrust_MainActivity_helloNameFromJNI(
-   env: JNIEnv,
-   _this: JObject,
-   input: JString,
+    env: JNIEnv,
+    _this: JObject,
+    input: JString,
 ) -> jstring {
     // intoはFromの逆の働き
     // この場合はJavaStrをStringに変換してくれている
@@ -44,4 +44,75 @@ pub unsafe extern "C" fn Java_com_kokoadev_ndkwithrust_MainActivity_addNumberFro
     let num2 = input2 as i32;
     let result = num1 + num2;
     result as jint
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn Java_com_kokoadev_ndkwithrust_MainActivity_sortIntArrayFromJNI(
+    env: JNIEnv,
+    _this: JObject,
+    list: jintArray,
+) -> jintArray {
+
+    //get_int_array_elementsでintArrayを取る
+    let auto_array = env.get_int_array_elements(list, ReleaseMode::CopyBack).unwrap();
+    let len = auto_array.size().unwrap() as usize;
+    let slice = unsafe { std::slice::from_raw_parts_mut(auto_array.as_ptr(), len) };
+    let mut vec = Vec::from(slice);
+
+    vec.sort();
+    vec.sort_by_bubble_algorithm();
+    // bubble_sort(&mut vec);
+    quick_sort(&mut vec);
+    let int_array = env.new_int_array(vec.len() as i32).unwrap();
+    env.set_int_array_region(int_array, 0, &vec).unwrap();
+
+    int_array
+}
+
+
+fn bubble_sort(arr: &mut [jint]) {
+    let len = arr.len();
+    for i in 0..len {
+        for j in 0..(len - i - 1) {
+            if arr[j] > arr[j + 1] {
+                arr.swap(j, j + 1)
+            }
+        }
+    }
+}
+
+fn quick_sort(arr: &mut [jint]) {
+    if arr.len() > 1 {
+        let pivot = partition(arr);
+        quick_sort(&mut arr[..pivot]);
+        quick_sort(&mut arr[(pivot + 1)..]);
+    }
+}
+
+fn partition(arr: &mut [jint]) -> usize {
+    let len = arr.len();
+    let pivot_index = len / 2;
+    let mut i = 0;
+    let j = len - 1;
+    arr.swap(pivot_index, j);
+    for k in 0..(len - 1) {
+        if arr[k] < arr[j] {
+            arr.swap(k, i);
+            // i はpivotより小さい数値のカウント
+            i += 1;
+        }
+    }
+    // 最後のpivotを元に戻すことで左に小さい値、右に大きい値が並ぶ
+    arr.swap(i, j);
+    i
+}
+
+trait MyBubbleSort {
+    fn sort_by_bubble_algorithm(&mut self);
+}
+
+impl MyBubbleSort for [jint] {
+    fn sort_by_bubble_algorithm(&mut self) {
+        bubble_sort(self)
+    }
 }
